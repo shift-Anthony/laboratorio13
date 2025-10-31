e7136c4cd519df868a92d8a2f336f3714852d3d7
shift-Anthony <anthonymoisescondori@gmail.com>
2025-10-31 15:36:25 -0500

    add eje9

---
+++ javascript/ej9.js
@@ -0,0 +1,15 @@
+function contarPalabras(texto) {
+    let contar = new Map(), resultado = "";
+    palabras = texto.split(" ");
+    for (palabra of palabras) {
+        if (contar.has(palabra)) contar.set(palabra, contar.get(palabra) + 1);
+        else contar.set(palabra, 1);
+    }
+    contar.forEach(function (cantidad, palabra) { resultado += `${palabra} => ${cantidad}\n` })
+    return resultado;
+}
+//Ejecucion del Codigo
+const oracion = "hola mundo hola universo mundo hola";
+const oracion2 = "sol luna sol sol estrella luna";
+alert(`Texto Ingresado: ${oracion}\nEl map retorna:\n${contarPalabras(oracion)}`);
+alert(`Texto Ingresado: ${oracion2}\nEl map retorna:\n${contarPalabras(oracion2)}`);
